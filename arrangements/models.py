887fd89c5509a37ec9e306eda2abbdad4c40e5c4
江昊天mac <thss15_jianght@163.com>
2017-06-29 00:33:27 +0800

    修正

--- arrangements/models.py
+++ arrangements/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class UserInfo(models.Model):
     user = models.OneToOneField('auth.user', related_name='user_info')
-    nickname = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, unique=True)
     MALE = 'M'
     FEMALE = 'F'
     GENDER_CHOICES = (
